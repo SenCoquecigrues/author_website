b8577c1951c9c96a5161d634367098307e90be25
Lucile GB <lgarrigoux@hotmail.com>
2026-07-12 16:06:59 +0200

    chore: update core tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,22 +3,32 @@ from django.test import TestCase
 
 from accounts.models import Member
 
-
+"""
+    This tests all main parts of the website: the library,
+    the blog, the about page, the error pages, etc.
+"""
 class CoreTestCase(TestCase):
-    def setUp(self):
-        Member.objects.create_user("Jean-Bob", email="jeanbobdupont@mail.fr", password="motdepasse")
-
-    def test_page_does_not_exist(self):
+    def test_404(self):
         response = self.client.get('/url_does_not_exists')
         self.assertContains(response, 'Malheureusement, cette page n\'existe pas !')
         self.assertEqual(response.status_code, 200)
 
-    def test_homepage_view(self):
-        response = self.client.get(reverse("homepage"))
-        self.assertContains(response, 'Autrice M/M | Romans BL et fantasy')
+    def test_voiture_noire_view(self):
+        response = self.client.get(reverse("voiture_noire:index"))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse("library:index"))
+
+    def test_library_view(self):
+        response = self.client.get(reverse("library:index"))
+        self.assertContains(response, 'Petite Voiture Noire')
         self.assertEqual(response.status_code, 200)
 
-    def test_about(self):
+    def test_homepage_view(self):
+        response = self.client.get(reverse("homepage"))
+        self.assertContains(response, 'My Name, grande autrice')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_view(self):
         response = self.client.get(reverse("about"))
         self.assertContains(response, 'Bibliographie')
         self.assertEqual(response.status_code, 200)
@@ -26,9 +36,4 @@ class CoreTestCase(TestCase):
     def test_gadget_view(self):
         response = self.client.get(reverse("gadgets:index"))
         self.assertContains(response, 'Coquecigrues : trucs, gadgets et machins')
-        self.assertEqual(response.status_code, 200)
-
-    def test_library_view(self):
-        response = self.client.get(reverse("voiture_noire:index"))
-        self.assertContains(response, 'Une Petite Voiture Noire')
         self.assertEqual(response.status_code, 200)
